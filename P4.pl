2e12301ce6371bcbec02eb38aa941bc55378c3c0
Dangonizer <schierkevin@gmail.com>
2024-06-19 17:57:01 +0200

    P4

---
+++ P4.pl
@@ -0,0 +1,39 @@
+%spezieller NEA für L_end01
+
+%Zustandsmenge Z
+%zustand(Z): Z ist Teil der Zustandsmenge
+zustand(z0).
+zustand(z1).
+zustand(z2).
+
+%Alphabet
+%sigma(X): X ist Teil des Alphabets.
+sigma(t0).
+sigma(t1).
+
+%Transitionsfunktionen
+%delta(AktZ,A,NeuZ): Transition von AktZ zu NeuZ über das Zeichen A.
+delta(z0,t0,z1).
+delta(z0,t0,z0).
+delta(z0,t1,z0).
+delta(z1,t1,z2).
+
+%Startzustandsmenge Z0
+%start(Z): Z ist Teil der Startzustandsmenge.
+start(z0).
+
+%Endzustandsmenge E
+%end(Z): Z ist Teil der Endzustandsmenge.
+end(z2).
+
+%Allgemeiner Teil
+%sigma_stern(Ws): Ws ist ein gültiges Wort über dem Alphabet Sigma.
+sigma_stern([]).
+sigma_stern([A|Ws]) :- sigma(A),sigma_stern(Ws).
+
+%delta_stern(AktZ,Ws,NeuZ): Transition vom Zustand AktZ zu NeuZ über das Wort Ws.
+delta_stern(AktZ,[],AktZ).
+delta_stern(AktZ,[A|Ws],NeuZ) :- delta(AktZ,A,HelpZ), delta_stern(HelpZ,Ws,NeuZ).
+
+%lvonN(Ws): Wort Ws ist gültiges Wort der Sprache die durch den NEA beschrieben wird.
+lvonN(Ws) :- start(StartZ), end(EndZ), delta_stern(StartZ,Ws,EndZ), sigma_stern(Ws).
